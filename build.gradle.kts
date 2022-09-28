a2980ea2e7b370113758a7a9bbdf992f1246d65b
Sidharth <sidharthks60@gmail.com>
2022-09-28 12:52:55 +0530

    Final Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ group = "com.employee"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
 repositories {
 	mavenCentral()
 }
